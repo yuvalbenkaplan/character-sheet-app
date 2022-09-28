412648bf51e74907357f59ac663d40e81e65f297
unknown <yuvalbenkaplan@gmail.com>
2022-09-27 20:32:01 -0400

    modified character schema

--- models/Character.js
+++ models/Character.js
@@ -17,6 +17,30 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  Strength: {
+    type: Number,
+    required: true,
+  },
+  Dexterity: {
+    type: Number,
+    required: true,
+  },
+  Constitution: {
+    type: Number,
+    required: true,
+  },
+  Intelligence: {
+    type: Number,
+    required: true,
+  },
+  Wisdom: {
+    type: Number,
+    required: true,
+  },
+  Charisma: {
+    type: Number,
+    required: true,
+  },
   level: {
     type: Number,
     required: true,
